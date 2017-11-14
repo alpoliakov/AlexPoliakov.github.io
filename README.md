f8fd4a904416c26f9aab95e7efb34884de732222
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-11-14 22:13:34 +0200

    "Placing a game "Memory - Pair Game" on the Github Pgs"

---
+++ README.md
@@ -0,0 +1,7 @@
+# "Memory - Pair Game"
+
+--------------------
+
+>__"Memory - Pair Game" - a card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards. The game ends when the last pair has been picked up.__
+
+--------------------
---
+++ scripts/app.js
@@ -0,0 +1,138 @@
+class Game {
+    constructor(elem, count, countCards, buttonElem) {
+        this.elem = elem;
+        this.steps = count;
+        this.countCards = countCards;
+        this.elemSteps = document.body.querySelector('.score');
+        this.elemSteps.innerHTML = 0;
+
+        this.buttonElem = buttonElem;
+        this.buttonElem.innerHTML = 'Clear and restart';
+
+        this.openCards = new Set();
+
+        this.flipElem = this.flipElem.bind(this);
+        this.elem.addEventListener('click', this.flipElem);
+    }
+
+    flipElem(evt) {
+        this.el = evt.target;
+        if (!this.el.classList.contains('front')) return;
+        this.el.parentNode.classList.toggle('flipped');
+        this.steps += 1;
+        this.elemSteps.innerHTML = this.steps;
+
+        if (this.openCards.size > 0) {
+            this.openCards.forEach(elem => {
+                if (elem.dataset.name === this.el.parentNode.dataset.name) {
+                    setTimeout(() => {
+                        elem.remove();
+                        this.el.parentNode.remove();
+                    }, 800);
+                    this.openCards.delete(elem);
+                    this.countCards -= 2;
+                }
+            });
+
+            if (this.openCards.size > 1) {
+                this.openCards.forEach(elem => {
+                    elem.classList.toggle('flipped');
+                });
+                this.openCards.clear();
+            }
+
+            if (this.countCards === 0) {
+                setTimeout(() => {
+                    alert('You Win!');
+                }, 1500);
+
+                this.buttonElem.innerHTML = 'Start the game again?';
+            }
+        }
+
+        this.openCards.add(this.el.parentNode);
+    }
+}
+
+const initialGame = (parenElem, count, countCards) => {
+    const buttonElem = document.querySelector('.start__button');
+    return new Game(parenElem, count, countCards, buttonElem);
+};
+
+
+class BuildPlayingField {
+    constructor({elem, classButton, fieldParent, countSteps, elemSteps, arrayAttributes}) {
+        this.elem = elem;
+        this.classButton = classButton;
+        this.fieldParent = fieldParent;
+        this.countSteps = countSteps;
+        this.elemSteps = elemSteps;
+        this.arrAttr = arrayAttributes;
+        this.countCards = this.arrAttr.length;
+
+        this.beginGame = this.beginGame.bind(this);
+        this.elem.addEventListener('click', this.beginGame);
+    }
+
+    beginGame(evt) {
+        this.el = evt.target;
+        if (this.el.className !== this.classButton) return;
+
+        if (this.fieldParent.lastChild) {
+            this.fieldParent.remove();
+            this.fieldParent = document.createElement('div');
+            this.fieldParent.classList.add('playing__field');
+            document.querySelector('.content').append(this.fieldParent);
+            this.countSteps = 0;
+        }
+
+        this.arrAttr.sort(function() { return 0.5 - Math.random() });
+        let template = ``;
+        for (let count = 0; count < this.arrAttr.length; count++) {
+            template += `<section class="container__flip">
+                            <div class="card" data-name=${this.arrAttr[count].dataset}>
+                                <div class="front"></div> 
+                                <div class="back">
+                                    <img src=${this.arrAttr[count].path}>
+                                </div>                                                          
+                             </div>
+                        </section>`;
+        }
+        this.fieldParent.insertAdjacentHTML(`afterBegin`, template);
+        document.body.querySelector('.text__score').hidden = false;
+
+        initialGame(this.fieldParent, this.countSteps, this.countCards, this.buttonElem);
+    }
+
+}
+
+const initialTask = () => {
+    const value = {
+        elem: document.querySelector('.content'),
+        classButton: 'start__button',
+        fieldParent: document.querySelector('.playing__field'),
+        countSteps: 0,
+        arrayAttributes: [
+            {dataset: "linux", path: "img/linux.svg"},
+            {dataset: "linux", path: "img/linux.svg"},
+            {dataset: "google", path: "img/google.svg"},
+            {dataset: "google", path: "img/google.svg"},
+            {dataset: "html5", path: "img/html5.svg"},
+            {dataset: "html5", path: "img/html5.svg"},
+            {dataset: "firefox", path: "img/firefox.svg"},
+            {dataset: "firefox", path: "img/firefox.svg"},
+            {dataset: "apple", path: "img/apple.svg"},
+            {dataset: "apple", path: "img/apple.svg"},
+            {dataset: "vue", path: "img/vue.svg"},
+            {dataset: "vue", path: "img/vue.svg"},
+            {dataset: "webpack", path: "img/webpack.svg"},
+            {dataset: "webpack", path: "img/webpack.svg"},
+            {dataset: "github", path: "img/github.svg"},
+            {dataset: "github", path: "img/github.svg"}
+        ]
+    };
+
+    return new BuildPlayingField(value);
+};
+
+initialTask();
